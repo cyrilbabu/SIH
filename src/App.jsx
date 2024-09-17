63f28a00e7902b8dff4fcb42913cd1254ae18f1c
Priyanshu Kushwaha <priyanshukushwaha311@gmail.com>
2024-09-17 12:18:18 +0530

    login signup

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,97 @@
+/* eslint-disable no-unused-vars */
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { ToastContainer } from "react-toastify";
+import { Toaster } from "react-hot-toast";
+import Login from "./Common/auth/login";
+import Signup from "./Common/auth/Signup";
 
-function App() {
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 0,
+    },
+  },
+});
 
-
+export default function App() {
   return (
-    <>
-     <h1 className="text-center bg-slate-300 text-white text-4xl">Smart India Hackathon</h1>
-    </>
-  )
-}
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
 
-export default App
+          <Route path="/" element={<Login />} />
+          {/* <Route path="*" element={<PageNotFound />} /> */}
+          {/* ------------------------------- User  Routes ------------------------------- */}
+          {/* <Route
+            element={
+              <PrivateRoute>
+              <EmployeeAppLayout />
+              </PrivateRoute>
+            }
+          ></Route> */}
+          {/* ------------------------------- Admin  Routes ------------------------------- */}
+          {/* <Route
+            element={
+              <Protected>
+              <AdminAppLayout />
+              </Protected>
+            }
+          ></Route> */}
+        </Routes>
+        <ToastContainer
+          position="top-center"
+          autoClose={5000}
+          hideProgressBar={true}
+          newestOnTop={true}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+          closeButton={false}
+          style={{
+            fontSize: "16px",
+            maxWidth: "1000px",
+            margin: "0 50px",
+            padding: "16px 24px",
+            backgroundColor: "var(--color-grey-0)",
+            color: "black",
+          }}
+          toastStyle={{
+            backgroundColor: "white",
+            color: "black",
+            padding: "16px 24px",
+          }}
+        />
+
+        <Toaster
+          position="top-center"
+          gutter={12}
+          containerStyle={{ margin: "8px" }}
+          toastOptions={{
+            duration: 5000, // Matches autoClose duration
+            style: {
+              fontSize: "16px",
+              maxWidth: "700px",
+              margin: "0 50px",
+              padding: "16px 24px",
+              backgroundColor: "white",
+              color: "black",
+            },
+            success: {
+              duration: 3000, // Specific for success
+            },
+            error: {
+              duration: 5000, // Specific for error
+            },
+          }}
+        />
+      </BrowserRouter>
+    </QueryClientProvider>
+  );
+}
