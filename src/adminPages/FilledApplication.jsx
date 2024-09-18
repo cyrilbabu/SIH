83fd09202ab08a7893d15b1484fd22523a736aba
Priyanshu Kushwaha <priyanshukushwaha311@gmail.com>
2024-09-18 14:33:56 +0530

    update user detail

---
+++ src/adminPages/FilledApplication.jsx
@@ -0,0 +1,17 @@
+import UserApplication from "../componemts/UserApplication";
+
+export default function FilledApplication() {
+  return (
+    <div className="w-full p-4">
+      <div className="w-full pb-4">
+        <h1 className="text-4xl font-bold ">Application Filled</h1>
+      </div>
+      <div className="grid grid-cols-2 gap-2">
+        <UserApplication />
+        <UserApplication />
+        <UserApplication />
+        <UserApplication />
+      </div>
+    </div>
+  );
+}
